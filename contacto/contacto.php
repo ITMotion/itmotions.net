7d6d0e7fa26bb8ff0a2420ef0c6371933fb7779f
bstsebastian <sebastian.paredes@hotmail.es>
2016-01-08 23:40:56 -0500

    Diseño 1
    Adelanto de proyecto

---
+++ contacto/contacto.php
@@ -0,0 +1,36 @@
+﻿<html>
+<head>
+	<title>Contacto</title>
+	<link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css"/>
+	<link href="../css/style.css" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css1/main.css"/>
+	<meta name="viewport" content="width= device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<div class="container">
+		<div class="row" id="header">
+			<div class="col-md-2" id="logo">
+				<img src="../imagenes/logo2fondo.png"/>
+			</div>
+			<div class="col-md-10" id="nav">
+				<div class="nav nav-tabs">
+					<ul>
+						<li><a href="../index.php">Inicio</a></li>
+						<li><a href="../quienes somos/quienes somos.php">Quienes Somos</a></li>
+						<li><a href="../proyectos/proyectos.php">Proyectos</a></li>
+						<li><a href="../contacto/contacto.php">Contacto</a></li>
+					</ul>
+				</div>
+
+			</div>
+		</div>
+
+
+
+	</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
