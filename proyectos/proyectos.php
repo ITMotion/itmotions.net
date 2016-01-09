272a70a53fa1c73e931535f6009a73f441c921bb
bstsebastian <sebastian.paredes@hotmail.es>
2016-01-09 14:51:28 -0500

    Cambio de Thumbnails, Fixes #4
    Cambios de sebas

--- proyectos/proyectos.php
+++ proyectos/proyectos.php
@@ -1,9 +1,9 @@
 ﻿<html>
 <head>
-	<title>Quienes Somos</title>
+	<title>Proyectos</title>
 	<link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css"/>
-	<link href="../css/style.css" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="css1/main.css"/>
+	<link href="../css/style.css" rel="stylesheet">
 	<meta name="viewport" content="width= device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 <body>
@@ -24,51 +24,86 @@
 
 			</div>
 		</div>
+
 		<br>
 
 		<div class="row">
 			<div class="col-md-12" id="contenido1">
 				<div class="jumbotron">
-					<h1>Quienes Somos</h1>
+					<h1>Empieza a planear tu proyecto</h1>
 						<p>
-							Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+							Estas en manos de profecionales, se llevará de la mano todo el desarrollo con un equipo calificado y con los mejores estándares. Usted puede estar seguro que su proyecto tendra las caracteristicas mas altas de calidad de software.
 						</p>
 						<p>
-							Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-						</p>
+							Solo hace falta empezar a planear un futuro brillante... 
+						</p>	
+							<a href="#ventana" class="btn btn-primary" data-toggle="modal">aqui »</a>
+
+								<div class="modal fade" id="ventana" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+												<h4 class="modal-title" id="myModalLabel">contacto</h4>
+											</div>
+											<div class="modal-body">
+
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+											</div>
+										</div>
+									</div>
+								</div>
+							
+							
+						
 				</div>
 			</div>
 		</div>
 
-	
-			<div class="row">
-				<div class="col-md-4" id="mision">
+		<div class="row">
+			<div class="col-md-12" id="contenido2">
+				<h2>Un poco de nosotros</h2>
+					<p>
+						Desarrollo creativo en manos de jovenes con experiencia. Este proyecto "ITMotions" nace del aprendizaje, conocimiento y desarrollo en el area. En un principio el desarrollo era para plataformas web.
+					</p>
+					<p>
+						Hoy, gracias al desempeño y conocimiento exponencialmente estamos en crecimiento. Algunos de nuestros trabajos son:
+					</p>
+			</div>
+		</div>
+
+		<br>
+
+		<div class="row">
+				<div class="col-md-4" id="thumb1">
 					<div class="thumbnail">
-						<img src="../imagenes/mision.jpg"/>
+						<img src="../imagenes/pia.png"/>
 						<div class="caption">
-							<h3>Misión</h3>
+							<h3>PIA</h3>
 							<p>
 								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
 							</p>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-4" id="vision">
+				<div class="col-md-4" id="thumb2">
 					<div class="thumbnail">
-						<img src="../imagenes/vision.jpg"/>
+						<img src="../imagenes/pizza.png"/>
 						<div class="caption">
-							<h3>Visión</h3>
+							<h3>restaurante</h3>
 							<p>
 								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
 							</p>
 						</div>
 					</div>
 				</div>
-				<div class="col-md-4" id="valores">
+				<div class="col-md-4" id="thumb3">
 					<div class="thumbnail">
-						<img src="../imagenes/valores2.jpg" />
+						<img src="../imagenes/publi.jpg" />
 						<div class="caption">
-							<h3>Valores</h3>
+							<h3>PubliPrint</h3>
 							<p>
 								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
 							</p>
@@ -76,9 +111,8 @@
 					</div>
 				</div>
 		</div>
-	
 
-	<div class="row" id="footer">
+		<div class="row" id="footer">
 		<div class="col-md-12">
 			<div class="nav nav-tabs" align="center">
 				<ul>
@@ -91,10 +125,12 @@
 			</div>
 		</div>
 	</div>
+
+
 	</div>
 
 <script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>   
+<script src="js/bootstrap.min.js"></script>
 <script src="js/scripts.js"></script>
 </body>
 </html>
